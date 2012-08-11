6c3abf7d64c8f5fb0df81c40d74214f6000e8a59
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2012-08-11 14:33:48 -0300

    Evolution

--- src/sgate/db/evolutions/03.sql
+++ src/sgate/db/evolutions/03.sql
@@ -1,9 +1,10 @@
 # --- !Ups
 
 ALTER TABLE "sgate"."planos" DROP "preco";
-ALTER TABLE "sgate"."planos" ADD "preco" real not null;
+ALTER TABLE "sgate"."planos" ADD "preco" real not null default '0';
 
 # --- !Downs
 
-ALTER TABLE "sgate"."planos" DROP "preco";
 ALTER TABLE "sgate"."planos" ADD "preco" numeric(2) not null;
+ALTER TABLE "sgate"."planos" DROP "preco";
+
